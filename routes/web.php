e9e1af1424c5d4579152aea6bbf36dc59e39d0fc
Christian Herdiyanto Prasetia <christianherdiyanto@gmail.com>
2023-11-03 12:52:09 +0700

    semmbarang

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/register', [regisController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [regisController::class, 'register']);
 
-Route::get('/mata_kuliah', [MataKuliahController::class, 'index']);
-Route::get('/mata_kuliah/{IDMK}', [MataKuliahController::class, 'show'])->name('mata_kuliah.show');
-// Route::resource('/mata_kuliah', MataKuliahController::class);
+// Route::get('/mata_kuliah', [MataKuliahController::class, 'index']);
+// Route::get('/mata_kuliah/{IDMK}', [MataKuliahController::class, 'show'])->name('mata_kuliah.show');
+Route::resource('/mata_kuliah', MataKuliahController::class);
+
