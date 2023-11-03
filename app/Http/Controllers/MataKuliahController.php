f567925ab3146733013459336a51053f181a3c89
chudriana <135962199+chudriana@users.noreply.github.com>
2023-11-03 10:19:55 +0700

    Add MataKuliah

--- app/Http/Controllers/MataKuliahController.php
+++ app/Http/Controllers/MataKuliahController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MataKuliah;
 
 class MataKuliahController extends Controller
 {
     public function index(){
-        return view("layouts.mata_kuliah.index");
+        // codingan buat dapetin data dari model
+        $mataKuliah = MataKuliah::all();
+        var_dump($mataKuliah);die;
+
+        return view("layouts.mata_kuliah.index", compact('mataKuliah'));
     }
+    
 }
