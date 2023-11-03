a66b9bc1f9dab6ac6efa1ee3fac48054720cee59
chudriana <135962199+chudriana@users.noreply.github.com>
2023-11-03 10:22:32 +0700

    Revert "Add MataKuliah"
    This reverts commit f567925ab3146733013459336a51053f181a3c89.

--- app/Http/Controllers/MataKuliahController.php
+++ app/Http/Controllers/MataKuliahController.php
@@ -3,16 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\MataKuliah;
 
 class MataKuliahController extends Controller
 {
     public function index(){
-        // codingan buat dapetin data dari model
-        $mataKuliah = MataKuliah::all();
-        var_dump($mataKuliah);die;
-
-        return view("layouts.mata_kuliah.index", compact('mataKuliah'));
+        return view("layouts.mata_kuliah.index");
     }
-    
 }
