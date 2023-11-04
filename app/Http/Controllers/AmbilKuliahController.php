2ffb41b7da78551a4e1897e3477b43a6aa49473e
Christian Herdiyanto Prasetia <christianherdiyanto@gmail.com>
2023-11-05 00:22:23 +0700

    update ambil kuliah fix

--- app/Http/Controllers/AmbilKuliahController.php
+++ app/Http/Controllers/AmbilKuliahController.php
@@ -94,25 +94,26 @@ class AmbilKuliahController extends Controller
 
 
     public function update(Request $request, $NRP, $IDMK)
-    {
-        $item = DB::table('AmbilKuliah')
+{
+    $item = DB::table('AmbilKuliah')
+        ->where('NRP', $NRP)
+        ->where('IDMK', $IDMK)
+        ->first();
+
+    if ($item) {
+        $data = $request->validate([
+            'NilaiAngka' => 'required',
+        ]);
+
+        DB::table('AmbilKuliah')
             ->where('NRP', $NRP)
             ->where('IDMK', $IDMK)
-            ->first();
+            ->update($data);
 
-        if ($item) {
-            $data = $request->validate([
-                'NilaiAngka' => 'required',
-            ]);
-
-            DB::table('AmbilKuliah')
-                ->where('NRP', $NRP)
-                ->where('IDMK', $IDMK)
-                ->update($data);
-
-            return redirect()->route('ambil_kuliah.index')->with('success', 'Ambil Kuliah berhasil diedit!');
-        } else {
-        }
-    }
+        return redirect()->route('ambil_kuliah.index')->with('success', 'Ambil Kuliah berhasil diedit!');
+    } else {
+        return redirect()->route('ambil_kuliah.index')->with('error', 'Ambil Kuliah tidak ditemukan!');
+    }
+}
 
 }
