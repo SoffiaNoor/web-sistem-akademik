ddf0081c79efad35624eef4d72b9b7d423b918e8
SoffiaNoor <soffianaisyah0@gmail.com>
2023-11-09 08:56:48 +0700

    *revisi dikit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -235,7 +235,7 @@
                     </div>
                     <ul class="navbar-nav  justify-content-end">
                         <li class="nav-item d-flex align-items-center">
-                            <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
+                            <a href="/user/{{{$loggedInUser->id}}}" class="nav-link text-white font-weight-bold px-0">
                                 <i class="fa fa-user me-sm-1"></i>
                                 <span class="d-sm-inline d-none">{{ ucfirst($loggedInUser->name) }}</span>
                             </a>
